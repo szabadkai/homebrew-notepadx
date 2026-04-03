4884f72025e215b0cdb4bc3d34df8a3519349947
Levente Szabadkai <levente@szabadkai.com>
2026-04-03 12:28:02 +0200

    Update to v1.3.2 with real SHA256 hashes

--- Casks/notepadx.rb
+++ Casks/notepadx.rb
@@ -1,14 +1,14 @@
 cask "notepadx" do
-  version "1.0.6"
+  version "1.3.2"
 
   on_intel do
     url "https://github.com/szabadkai/NotepadX/releases/download/v#{version}/NotepadX-macos-x86_64.zip"
-    sha256 "PLACEHOLDER_INTEL_SHA256"
+    sha256 "cc68121dad8a8752fbc40235b2a764e5309e520c9c5f2a56c387b57712ff14e8"
   end
 
   on_arm do
     url "https://github.com/szabadkai/NotepadX/releases/download/v#{version}/NotepadX-macos-arm64.zip"
-    sha256 "PLACEHOLDER_ARM_SHA256"
+    sha256 "fce39dda59dbd3aea2108af686327a56b4714e2240d90b35337399ef47fdf21a"
   end
 
   name "NotepadX"
--- Formula/notepadx.rb
+++ Formula/notepadx.rb
@@ -1,8 +1,8 @@
 class Notepadx < Formula
   desc "Fast, beautiful, cross-platform text editor"
   homepage "https://github.com/szabadkai/NotepadX"
-  url "https://github.com/szabadkai/NotepadX/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "PLACEHOLDER_SOURCE_SHA256"
+  url "https://github.com/szabadkai/NotepadX/archive/refs/tags/v1.3.2.tar.gz"
+  sha256 "af058f918c00a5d93d21ed94efb51dfaa1ddda7a03277ef6a64863e5bddb7012"
   license "MIT"
 
   depends_on "rust" => :build
